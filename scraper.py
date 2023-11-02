4f70af64da2e1f2d6fdd6a4c2d7b5b08115fef22
EnesBrt <enes.barut@gmx.com>
2023-11-02 12:25:51 +0100

    upadated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,45 +1,49 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import selenium.webdriver
-# from webdriver_manager.safari import SafariDriverManager
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
 
+# Configurez Selenium avec Safari
 driver = webdriver.Safari()
 
-url = 'https://fr.indeed.com/jobs?q=Python&l=&from=searchOnHP&vjk=61067fdaf7f84d71'
+# URL de départ
+url = 'https://fr.indeed.com/jobs?q=Python&fromage=1&vjk=1b833041c0b8707f'
 driver.get(url)
 
+jobs = []
+ # Obtenez le code source de la page actuelle
 html = driver.page_source
-
 soup = BeautifulSoup(html, 'html.parser')
 
-jobs = []
-
+# Trouvez toutes les cartes de travail sur la page actuelle
 job_cards = soup.find_all('td', class_='resultContent')
 
-    # Iterate through each job card and scrape the required information
-for card in job_cards:
-    
-    job_title = card.find('h2', class_='jobTitle').get_text(strip=True)
-    
-    company_name_element = card.find('span', {'data-testid': 'company-name'})
-    company_name = company_name_element.get_text(strip=True) if company_name_element else 'Non spécifié'
-    
-    location_element = card.find('div', {'data-testid': 'text-location'})
-    location = location_element.get_text(strip=True) if location_element else 'Non spécifié'
-    
-    job_type_element = card.find('div', {'data-testid': 'attribute_snippet_testid'})
-    job_type = job_type_element.get_text(strip=True) if job_type_element else 'Non spécifié'
-    jobs.append({
-        'Job Title': job_title,
-        'Company Name': company_name,
-        'Location': location,
-        'Job Type': job_type
-    })
 
-# Don't forget to close the browser once done
+def extract_jobs(job_cards):
+
+# Extrayez les informations de chaque carte de travail
+    for card in job_cards:
+        job_title = card.find('h2', class_='jobTitle').get_text(strip=True)
+        company_name_element = card.find('span', {'data-testid': 'company-name'})
+        location_element = card.find('div', {'data-testid': 'text-location'})
+        job_type_element = card.find('div', {'data-testid': 'attribute_snippet_testid'})
+
+        jobs.append({
+            'Job Title': job_title if job_title else 'Non spécifié',
+            'Company Name': company_name_element.get_text(strip=True) if company_name_element else 'Non spécifié',
+            'Location': location_element.get_text(strip=True) if location_element else 'Non spécifié',
+            'Job Type': job_type_element.get_text(strip=True) if job_type_element else 'Non spécifié'
+        })
+            
+
 driver.quit()
 
-# At this point, `jobs` is a list of dictionaries with the scraped data from each job card
-for job in jobs:
-    print(job)
+
+
+
+if __name__ == '__main__':
+    extract_jobs(job_cards)
+    # Affichez ou traitez les emplois extraits
+    for job in jobs:
+        print(job)
